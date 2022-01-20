83b57f4e5e3b3b8db23f1054e680ce91e2990026
Kristina Kadagytė <kristinakadagyte@gmail.com>
2022-01-21 01:05:06 +0200

    Add project files.

---
+++ SchoolApplication/Models/School.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApplication.Models
+{
+    public class School : NamedEntity
+    {
+        public DateTime Created { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ SchoolApplication/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApplication.Models
+{
+    public class Student : NamedEntity
+    {
+        public string Sex { get; set; }
+        public int SchoolId { get; set; }
+        public School School { get; set; }
+    }
+}
